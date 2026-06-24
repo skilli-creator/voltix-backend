15265a1228478ba9c53e32c50c6dbe7c3378d496
skilli-creator <kyalotonny6@gmail.com>
2026-06-24 13:11:58 +0300

    Fix OAuth scope: change from trading to read

--- services/deriv_oauth_service.py
+++ services/deriv_oauth_service.py
@@ -41,7 +41,6 @@ class DerivOAuthService:
             'response_type': 'code',
             'client_id': self.client_id,
             'redirect_uri': self.redirect_uri,
-            'scope': 'read',  # Using 'trading' scope
             'code_challenge': code_challenge,
             'code_challenge_method': 'S256',
             'state': state
